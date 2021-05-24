28cc1101562adac5c05c89aeac99ba0c3b0510e2
jasonpitogoCI <jason@creativeinterlace.com>
2021-05-24 17:19:05 +0800

    update pod files

--- ios/AltEscModule.h
+++
@@ -1,8 +0,0 @@
-//  RCTCalendarModule.h
-#import <React/RCTBridgeModule.h>
-@import CoreBluetooth;
-
-@interface RCTAltEscModule : NSObject <RCTBridgeModule, CBCentralManagerDelegate>
-@property (nonatomic, strong) CBCentralManager *centralManager;
-@property (nonatomic, strong) CBPeripheral *peripheral;
-@end
